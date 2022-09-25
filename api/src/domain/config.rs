603a78e48cc4a6c0c4e2193df78e3f6d818f9340
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2022-09-25 20:44:56 +0900

    環境変数を読み出せるようにした

---
+++ api/src/domain/config.rs
@@ -0,0 +1,18 @@
+use serde::Deserialize;
+use std::error::Error;
+
+pub struct Service {}
+
+#[derive(Deserialize, Debug)]
+pub struct Config {
+    pub switch_bot_token: String,
+    pub interphone_bot_id: String,
+    pub lock_bot_id: String,
+}
+
+impl Service {
+    pub fn load() -> Result<Config, Box<dyn Error>> {
+        let config = envy::from_env::<Config>();
+        Ok(config?)
+    }
+}
